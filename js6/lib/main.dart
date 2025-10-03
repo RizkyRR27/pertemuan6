443da21d3edac7a9bbdc6da08317a8a465c05239
RizkyRR27 <rizkyroza2005@gmail.com>
2025-10-03 10:33:00 +0700

    drawer

--- js6/lib/main.dart
+++ js6/lib/main.dart
@@ -19,6 +19,32 @@ class MyApp extends StatelessWidget {
             Icon(Icons.settings),
           ],
         ),
+         drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
         body: const Center(
           child: Text("Ini Body Aplikasi"),
         ),
